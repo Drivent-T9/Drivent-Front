9e78a0055b8100a2c0dfcf1d67d1ec555fceb787
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-05-15 09:08:12 -0400

    feat: implemented tickets

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,84 +1,158 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import useEnrollment from '../hooks/api/useEnrollment';
 import Guest from './GuestComponent';
+import useTicketTypes from '../hooks/api/useTicketTypes';
+import { toast } from 'react-toastify';
+import useSaveTicket from '../hooks/api/useSaveTicket';
+import { useNavigate } from 'react-router-dom';
 
 export default function Ticket() {
   const [buttonType, setButtonType] = useState(null);
   const [hotelType, setHotelType] = useState(null);
-  const [mode, setMode] = useState(null);
+  const [includesHotel, setIncludesHotel] = useState(null);
   const [remoteOrder, setRemoteOrder] = useState(null);
   const { enrollment } = useEnrollment();
+  const [value, setValue] = useState(0);
+  const [presencialOrder, setPresencialOrder] = useState(null);
+  const { ticketTypes } = useTicketTypes();
+  const [ticket, setTicket] = useState(null);
+  const { saveTicket } = useSaveTicket();
+  const navigate = useNavigate();
 
-  return (
-    <>    
-    <Title>Ingresso e Pagamento</Title>
-      {!enrollment ? <Guest /> : 
-      <>
-      <Subtitle>Primeiro, escolha sua modalidade de ingresso</Subtitle>
-        <Options>
-          <Button
-            buttonType={buttonType === true}
-            onClick={() => {
-              setButtonType(true)
-              setMode(true)
-              setRemoteOrder(false)
-            }}
-          >
-            <span>Presencial</span>
-            <p>R$ 250</p>
-          </Button>
-
-          <Button
-            buttonType={buttonType === false}
-            onClick={() => {
-              setButtonType(false)
-              setMode(false)
-              setRemoteOrder(true)
-            }}
-          >
-            <span>Online</span>
-            <p>R$ 100</p>
-          </Button>
-        </Options>
-
-      {remoteOrder ? 
-        <>
-          <Subtitle>Fechado! O total ficou em R$ 100. Agora é só confirmar:</Subtitle>
-          <FinishTicket>RESERVAR INGRESSO</FinishTicket>
-        </>
-      : <></>
+  useEffect(() => {
+    if (ticketTypes) {
+      if (remoteOrder === true) {
+        //online
+        setTicket(ticketTypes.filter((t) => t.isRemote === true));
       }
 
-      {mode ?
+      if (includesHotel === true) {
+        //presencial com hotel
+        setTicket(ticketTypes.filter((t) => t.includesHotel === true));
+      }
+
+      if (remoteOrder === false && includesHotel === false) {
+        //presencial sem hotel
+        setTicket(ticketTypes.filter((t) => t.isRemote === false && t.includesHotel === false));
+      }
+    }
+
+  },[remoteOrder, includesHotel])
+
+  async function createTicket() {
+    const data = {
+      ticketTypeId: ticket[0].id,
+    };
+
+    try {
+      await saveTicket(data);
+      toast('Ticket reservado!');
+      navigate('/dashboard/payment/credit-card');
+    } catch (error) {
+      // eslint-disable-next-line
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <Title>Ingresso e Pagamento</Title>
+      {!enrollment ? (
+        <Guest />
+      ) : (
         <>
-          <Subtitle>Ótimo! Agora escolha sua modalidade de hospedagem</Subtitle>
+          <Subtitle>Primeiro, escolha sua modalidade de ingresso</Subtitle>
           <Options>
             <Button
-              hotelType={hotelType === true}
+              buttonType={buttonType === true}
               onClick={() => {
-                setHotelType(true)
+                setButtonType(true);
+                setRemoteOrder(false);
+                setValue(250);
               }}
             >
-              <span>Sem Hotel</span>
-              <p>+ R$ 0</p>
+              <span>Presencial</span>
+              <p>R$ 250</p>
             </Button>
 
             <Button
-              hotelType={hotelType === false}
+              buttonType={buttonType === false}
               onClick={() => {
-                setHotelType(false)
+                setButtonType(false);
+                setIncludesHotel(false);
+                setRemoteOrder(true);
+                setValue(100);
+                setPresencialOrder(false);
               }}
             >
               <span>Online</span>
-              <p>+ R$ 350</p>
+              <p>R$ 100</p>
             </Button>
           </Options>
+
+          {remoteOrder ? (
+            <>
+              <Subtitle>Fechado! O total ficou em R${value}. Agora é só confirmar:</Subtitle>
+              <FinishTicket
+                onClick={createTicket}
+              >
+                RESERVAR INGRESSO
+              </FinishTicket>
+            </>
+          ) : (
+            <></>
+          )}
+
+          {remoteOrder === false ? (
+            <>
+              <Subtitle>Ótimo! Agora escolha sua modalidade de hospedagem</Subtitle>
+              <Options>
+                <Button
+                  hotelType={hotelType === true}
+                  onClick={() => {
+                    setHotelType(true);
+                    setValue(250);
+                    setPresencialOrder(true);
+                    setIncludesHotel(false);
+                  }}
+                >
+                  <span>Sem Hotel</span>
+                  <p>+ R$ 0</p>
+                </Button>
+
+                <Button
+                  hotelType={hotelType === false}
+                  onClick={() => {
+                    setHotelType(false);
+                    setValue(600);
+                    setPresencialOrder(true);
+                    setIncludesHotel(true);
+                  }}
+                >
+                  <span>Com Hotel</span>
+                  <p>+ R$ 350</p>
+                </Button>
+              </Options>
+            </>
+          ) : (
+            <></>
+          )}
+
+          {presencialOrder ? (
+            <>
+              <Subtitle>Fechado! O total ficou em R${value}. Agora é só confirmar:</Subtitle>
+              <FinishTicket
+                onClick={createTicket}
+              >
+                RESERVAR INGRESSO
+              </FinishTicket>
+            </>
+          ) : (
+            <></>
+          )}
         </>
-      : 
-        <></>}      
-      </>
-      }
+      )}
 
     </>
   );
@@ -91,7 +165,7 @@ const Title = styled.h1`
   line-height: 39.84px;
   width: 100%;
   color: black;
-`
+`;
 
 const Subtitle = styled.h2`
   margin: 15px 0 10px 0;
@@ -101,31 +175,31 @@ const Subtitle = styled.h2`
   line-height: 39.84px;
   width: 100%;
   color: gray;
-`
+`;
 
 const Options = styled.div`
   display: flex;
   margin-bottom: 40px;
   width: 37%;
   justify-content: space-between;
-`
+`;
 
 const Button = styled.button`
   box-sizing: border-box;
   height: 145px;
   width: 145px;
-  background-color: ${({ buttonType, hotelType }) => buttonType || hotelType ? '#FFEED2' : '#FFFFFF'};
-  border: 1px solid #CECECE;
+  background-color: ${({ buttonType, hotelType }) => (buttonType || hotelType ? '#FFEED2' : '#FFFFFF')};
+  border: 1px solid #cecece;
   border-radius: 20px;
 
   :hover {
     cursor: pointer;
   }
-`
+`;
 
 const FinishTicket = styled.button`
   width: 162px;
   height: 37px;
   border-radius: 10px;
-  border: 1px solid #E0E0E0;
-`
+  border: 1px solid #e0e0e0;
+`;
