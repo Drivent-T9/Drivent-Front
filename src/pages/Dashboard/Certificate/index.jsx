98a64ddcb4d5b0b08f1a7f4f620f2127e3f31d96
LPandorf <leticiapandorf@gmail.com>
2023-05-20 17:38:20 -0300

    feat: activities backlog static part

--- src/pages/Dashboard/Certificate/index.jsx
+++ src/pages/Dashboard/Certificate/index.jsx
@@ -2,14 +2,3 @@ export default function Certificate() {
   
   return 'Certificado: Em breve!';
 }
-
-/*const now=getDate();
-  if(now>getEventInfo.event.endsAt){
-    console.log("oiiiiiiiiiiii");
-  }
-  if(getEventInfo==null){
-    return 'NULO';
-  } const getEventInfo= useEvent();
-  console.log(getEventInfo.event.endsAt);import { EventInfoProvider } from "../../../contexts/EventInfoContext";
-import useEvent from "../../../hooks/api/useEvent";
-*/
